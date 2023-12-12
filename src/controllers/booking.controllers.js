12f570052f1ddf68d17575c0f50b9c8080889fc9
oorangeed <pgh9311@gmail.com>
2023-12-13 04:11:14 +0900

    feat: [booking-controller] 예약 CRUD router, controller 작성

--- src/controllers/booking.controllers.js
+++ src/controllers/booking.controllers.js
@@ -1,1 +1,114 @@
-export class BookingController {}
+import { BookingService } from "../services/booking.service.js"
+import { StatusCodes } from "../constants/statusCodes.constant.js";
+
+
+export class BookingController {
+
+    bookingService = new BookingService();
+
+    // 예약 생성
+    createBookingController = async (req, res, next) => {
+        try {
+            const { UserId } = req.user;
+            const { title, sitterId, content, bookedAt } = req.body;
+            const booking = await this.bookingService.createBookingService(
+                UserId,
+                title,
+                sitterId,
+                content,
+                bookedAt
+            );
+
+            return res.status(StatusCodes.CREATED).json({
+                success: true,
+                message: "예약이 완료되었습니다.",
+                data: booking
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
+
+
+    // 예약 전체 조회
+    getBookingListController = async (req, res, next) => {
+        try {
+            const { sort } = req.query;
+            const bookings = await this.bookingService.getBookingListService(
+                sort
+            );
+
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "전체 예약이 조회되었습니다.",
+                data: bookings,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
+
+
+    // 예약 상세 조회
+    getBookingIdController = async (req, res, next) => {
+        try {
+            const { bookingId } = req.params;
+            const booking = await this.bookingService.getBookingIdService(
+                bookingId
+            );
+
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "예약 상세페이지가 조회되었습니다.",
+                data: booking,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
+
+
+    // 예약 수정
+    updateBookingController = async (req, res, next) => {
+        try {
+            const { UserId } = req.query;
+            const { bookingId } = req.params;
+            const { title, sitterId, content, bookedAt } = req.body;
+            const updateBooking = await this.bookingService.updateBookingService(
+                UserId,
+                bookingId,
+                title,
+                sitterId,
+                content,
+                bookedAt
+            );
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "예약이 수정되었습니다.",
+                data: updateBooking,
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
+
+
+    // 예약 삭제
+    deleteBookingController = async (req, res, next) => {
+        try {
+            const { UserId } = req.query;
+            const { bookingId } = req.params;
+            const deleteBooking = await this.bookingService.deleteBookingService(
+                UserId,
+                bookingId);
+
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "예약이 삭제되었습니다."
+            });
+        } catch (err) {
+            next(err);
+        }
+    };
+
+}
--- src/routes/booking.router.js
+++ src/routes/booking.router.js
@@ -1,5 +1,25 @@
 import express from "express";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
+import { BookingController } from "../controllers/booking.controllers.js";
 
 const router = express.Router();
+
+const bookingController = new BookingController;
+
+// 신규 예약 생성
+router.post("/booking", AuthMiddleware, bookingController.createBookingController);
+
+// 예약 전체 조회
+router.post("/booking", bookingController.getBookingListController);
+
+// 예약 상세 조회
+router.post("/booking/:bookingId", bookingController.getBookingIdController);
+
+// 예약 수정
+router.post("/booking/:bookingId", AuthMiddleware, bookingController.updateBookingController);
+
+// 예약 삭제
+router.post("/booking/:bookingId", AuthMiddleware, bookingController.deleteBookingController);
+
 
 export default router;
