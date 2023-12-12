464fc34687483a56ec27873208ea28ecf3198622
=lemon-table <=>
2023-12-12 21:04:32 +0900

    fix: routes index.js 세부항목 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,17 @@ import express from "express";
 import ProductsRouter from "./products.router.js";
 import UsersRouter from "./users.router.js";
 import AuthRouter from "./auth.router.js";
+import BookingRouter from "./booking.router.js";
+import SitterRouter from "./sitter.router.js";
+import ReviewRouter from "./review.router.js";
 
 const router = express.Router();
 
 router.use("/products/", ProductsRouter);
 router.use("/users/", UsersRouter);
 router.use("/auth/", AuthRouter);
+router.use("/booking/", BookingRouter);
+router.use("/sitter/", SitterRouter);
+router.use("/review/", ReviewRouter);
 
 export default router;
