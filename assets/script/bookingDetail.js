08705e24b21c48240d43a545c1a04be6bbdcb137
=lemon-table <=>
2023-12-18 02:08:33 +0900

    전체페이지 css 수정

--- assets/script/bookingDetail.js
+++ assets/script/bookingDetail.js
@@ -141,7 +141,8 @@ window.addEventListener("load", async (event) => {
     if (response.ok) {
       const responseData = await response.json();
 
-      document.getElementById("headerTitle").textContent = responseData.data.user.name + "님의 예약 상세 페이지";
+      document.getElementById("headerTitle").textContent =
+        responseData.data.user.users.nickname + "님의 예약 작성 페이지";
 
       const getSitter = await getpetSitter();
       const sitterSelect = document.getElementById("sitter-select");
@@ -222,6 +223,31 @@ document.querySelector("#save-button").addEventListener("click", async (event) =
     const errorData = await response.json();
     alert(errorData.errorMessage);
   }
+});
+
+window.addEventListener("load", async (event) => {
+  //repeatScalingAnimation();
+  const scalingElement = document.querySelector(".header-image");
+
+  scalingElement.addEventListener("animationiteration", function () {
+    // 애니메이션 반복 시 배경 이미지 변경
+    const currentImage = scalingElement.style.backgroundImage;
+    let changeImageUrl = 'url("../images/dog1.png")';
+
+    if (currentImage.includes("dog1.png")) changeImageUrl = 'url("../images/dog2.png")';
+    else if (currentImage.includes("dog2.png")) changeImageUrl = 'url("../images/dog3.png")';
+    else scalingElement.style.backgroundImage = 'url("../images/dog1.png")';
+
+    scalingElement.style.backgroundImage = changeImageUrl;
+  });
+
+  // 페이지 로드 후 스케일 애니메이션 시작
+  scalingElement.classList.add("animated");
+});
+
+// 펫시터 매칭 서비스 제목 클릭시 메인페이지 이동
+document.querySelector(".header-image").addEventListener("click", function () {
+  location.href = "main.html";
 });
 
 /** 달력 포맷 지정 (예약한 날짜는 비활성화) */
